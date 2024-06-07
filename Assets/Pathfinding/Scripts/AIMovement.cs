87e589eb750c3ed27c6eb940d1e70f90b2df4753
therealsunx <sunilsapkota1224@gmail.com>
2024-06-07 22:50:04 +0545

    AI player follower added

--- Assets/Pathfinding/Scripts/AIMovement.cs
+++ Assets/Pathfinding/Scripts/AIMovement.cs
@@ -46,6 +46,7 @@ public class AIMovement : MonoBehaviour {
         stateText.text = mvState.ToString();
         switch(mvState){
             case MVSTATE.IDLE:
+                velocity.x *= 0.8f;
                 break;
             case MVSTATE.WALK:
                 WalkStateHandler();
@@ -66,25 +67,40 @@ public class AIMovement : MonoBehaviour {
         bool grounded = Physics2D.OverlapBox(groundCheck.position, groundCheck.localScale, 0f, groundLayer);
         Vector2 del = target - current;
 
-        if(grounded && del.y > 0f){
-            if(Mathf.Abs(del.x)<1.1f) {
-                velocity.x = del.x < 0f? speedX : -speedX;
-                return;
-            }
-            velocity.y = Mathf.Sqrt(-3f * del.y * Physics2D.gravity.y);
-            if(Mathf.Abs(del.x) > 1.9f){
-                float tan = 8f * del.y / del.x;
-                velocity.x = velocity.y / tan;
-            }else velocity.x = 0f;
-        } else {
-            if(del.y>0f) return;
-            if(Mathf.Abs(del.x) < 1.1f)
-                velocity.x = del.x > 0f? 2f : -2f;
-            else
+        if(grounded){
+            if(del.y > 0f){
+                if(Mathf.Abs(del.x) <= 1f){
+                    velocity.x = del.x < 0f? speedX : -speedX;
+                }else {
+                    velocity.y = Mathf.Sqrt(-2.8f * del.y * Physics2D.gravity.y);
+                    if(Mathf.Abs(del.x) < 1.5f) velocity.x = 0f;
+                    else velocity.x = -0.8f * del.x * Physics2D.gravity.y / velocity.y;
+                }
+            }else{
                 velocity.x = del.x > 0f? speedX : -speedX;
-            return;
+            }
+        }else if(del.y < -0.1f && Mathf.Abs(del.x)<1.5f){
+            velocity.x = del.x > 0f? speedX : -speedX;
         }
 
+//      if(grounded && del.y > 0f){
+//          if(Mathf.Abs(del.x)<1.1f) {
+//              velocity.x = del.x < 0f? speedX : -speedX;
+//              return;
+//          }
+//          velocity.y = Mathf.Sqrt(-3f * del.y * Physics2D.gravity.y);
+//          if(Mathf.Abs(del.x) > 1.9f){
+//              float tan = 8f * del.y / del.x;
+//              velocity.x = velocity.y / tan;
+//          }else velocity.x = 0f;
+//      } else {
+//          if(del.y>0f) return;
+//          if(Mathf.Abs(del.x) < 1.1f)
+//              velocity.x = del.x > 0f? 2f : -2f;
+//          else
+//              velocity.x = del.x > 0f? speedX : -speedX;
+//          return;
+//      }
     }
 
     void JumpDownHandler(){
@@ -93,61 +109,98 @@ public class AIMovement : MonoBehaviour {
 
         if(!grounded) return;
         if(del.y < -1.5f) velocity.x = del.x * Mathf.Sqrt(0.6f * Physics2D.gravity.y / del.y);
-        else velocity.x = del.x > 0f? speedX : -speedX;
+        else if (Mathf.Abs(del.x) > 3f){
+            velocity.x = Mathf.Sqrt(Mathf.Abs(del.x) * Physics2D.gravity.y * -0.4f);
+            velocity.y = velocity.x;
+            if(del.x < 0f) velocity.x = -velocity.x;
+        } else velocity.x = del.x > 0f? speedX : -speedX;
     }
 
     void JumpHorizontalHandler(){
         bool grounded = Physics2D.OverlapBox(groundCheck.position, groundCheck.localScale, 0f, groundLayer);
         Vector2 del = target - current;
         if(grounded) {
-            velocity.x = del.x * Physics2D.gravity.y * -0.1f;
-            velocity.y = Mathf.Abs(velocity.x);
-            if(velocity.magnitude > speedY) velocity = velocity.normalized * speedY;
+            velocity.x = Mathf.Sqrt(Mathf.Abs(del.x) * Physics2D.gravity.y * -0.5f);
+            velocity.y = velocity.x;
+            if(del.x < 0f) velocity.x = -velocity.x;
+            // if(velocity.magnitude > speedY) velocity = velocity.normalized * speedY;
         }
     }
 
     void WalkStateHandler(){
+        bool grounded = Physics2D.OverlapBox(groundCheck.position, groundCheck.localScale, 0f, groundLayer);
+        if(!grounded) return;
+
         Vector2 del = target - current;
-        velocity.x = del.x > 0f? speedX:-speedX;
+        if(Mathf.Abs(velocity.x)<.5f || Mathf.Abs(del.x) > 0.2f)
+            velocity.x = del.x > 0f? speedX : -speedX;
+    }
+
+    bool _NoWayCheck(){
+        if(path is null || curWayPoint >= (path.Count-1)){
+            mvState = MVSTATE.IDLE;
+            return true;
+        }
+        return false;
     }
 
     void PathStateCheck(){
-        if((curWayPoint + 1) >= path.Count) target = player.position;
-        else target = path[curWayPoint+1].position;
-        current = (Vector2) transform.position;
-        Vector2 del = target - current;
+        if(_NoWayCheck()) return;
 
-        // update current waypoint on graph based on next neighbours
-        Debug.DrawRay(current, del, del.magnitude < 1f?Color.green:Color.yellow);
-        if(del.magnitude > .1f){
-            mvState = mvState == MVSTATE.IDLE? MVSTATE.WALK : mvState;
-            return;
-        }
-        curWayPoint++;
+        target = path[curWayPoint+1].position; current = transform.position;
+        Vector2 del = target-current;
 
-        if((curWayPoint+1) >= path.Count){
-            target = player.position;
-            del = target - (Vector2) transform.position;
-            mvState = del.y >= .1f? MVSTATE.JUMPUP : MVSTATE.WALK;
-            Debug.Log("Path finished");
-            return;
-        }
+        System.Action _DetermineNextState = ()=>{
+            curWayPoint++;
+            if(_NoWayCheck()) return;
 
-        Node trg = path[curWayPoint+1], cur = path[curWayPoint];
-        target = trg.position;
-        del = target - cur.position;
+            Node cur = path[curWayPoint];
+            target = path[curWayPoint+1].position; current = transform.position;
+            del = target - cur.position;
 
-        if(Mathf.Abs(del.y) < 0.1f){
-            if(del.x > 0f){
-                mvState = (cur.type & (uint)NodeType.REDGE) > 0 ? MVSTATE.JUMPHOR : MVSTATE.WALK ;
-            }else{
-                mvState = (cur.type & (uint)NodeType.LEDGE) > 0 ? MVSTATE.JUMPHOR : MVSTATE.WALK ;
+            if(Mathf.Abs(del.y) < .1f){
+                if(del.x > 0f){
+                    mvState = (cur.type & (uint)NodeType.REDGE) > 0 ? MVSTATE.JUMPHOR : MVSTATE.WALK ;
+                }else{
+                    mvState = (cur.type & (uint)NodeType.LEDGE) > 0 ? MVSTATE.JUMPHOR : MVSTATE.WALK ;
+                }
+            } else if(del.y > 0f){
+                mvState = MVSTATE.JUMPUP;
+            } else {
+                mvState = Mathf.Abs(del.x) < 1.1f ? MVSTATE.WALK : MVSTATE.JUMPDOWN;
             }
-        }else if(del.y < 0f){
-            mvState = (del.y >= -2.1f && Mathf.Abs(del.x) > Mathf.Abs(del.y))?MVSTATE.JUMPHOR : MVSTATE.JUMPDOWN;
-        }else{
-            mvState = MVSTATE.JUMPUP;
-        }
+        };
+        
+        System.Action _AdjustToTarget = ()=>{
+            // first check for overshoot state
+            if((curWayPoint+2)<path.Count){
+                Node trg = path[curWayPoint+1], ntrg = path[curWayPoint+2];
+                Vector2 _dl = ntrg.position-trg.position;
+                if(Mathf.Abs(_dl.y)<0.1f){
+                    float _min, _max;
+                    if(trg.position.x > ntrg.position.x){
+                        _min = ntrg.position.x;
+                        _max = trg.position.x;
+                    }else{
+                        _min = trg.position.x;
+                        _max = ntrg.position.x;
+                    }
+
+                    if(current.x > _min && current.x < _max)
+                        _DetermineNextState();
+                    else if(Mathf.Abs(ntrg.position.x - current.x) < 0.6f*Mathf.Abs(trg.position.x - current.x))
+                        _DetermineNextState();
+                }
+            }else{
+                mvState = MVSTATE.WALK;
+            }
+        };
+
+        if( Mathf.Abs(del.y) < .1f && Mathf.Abs(del.x) < 0.1f)
+            _DetermineNextState();
+        else if ( Mathf.Abs(del.y) < 1f && Mathf.Abs(del.x) < 1f)
+            _AdjustToTarget();
+        else if (mvState == MVSTATE.IDLE) mvState = MVSTATE.WALK;
     }
 
     void UpdatePath(){
